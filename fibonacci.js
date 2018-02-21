0f6856826fc1c00943f11562c14851eb33e7551d
Michael Linderman <mlinderman@middlebury.edu>
2018-02-21 15:13:01 -0500

    Satisfy basic Fibonnaci tests

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,19 @@
 const fib = function fibonacci(num) {
+  let a = 0;
+  let b = 1;
+
   if (num === 0) {
-    return 0;
+    return a;
   } else if (num === 1) {
-    return 1;
+    return b;
   }
+
+  for (let i = 2; i <= num; i += 1) {
+    const c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 };
 
 module.exports = fib;
