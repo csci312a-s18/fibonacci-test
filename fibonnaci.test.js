f08bcf75c3a03b4a8a61472f2fb6efa54820567b
Michael Linderman <mlinderman@middlebury.edu>
2018-02-21 15:09:52 -0500

    Add basic Fibonnaci tests

--- fibonnaci.test.js
+++ fibonnaci.test.js
@@ -5,4 +5,10 @@ describe('Computes Fibonacci numbers', () => {
     expect(fib(0)).toBe(0);
     expect(fib(1)).toBe(1);
   });
+
+  test('Computes arbitrary Fibonacci numbers', () => {
+    expect(fib(2)).toBe(1);
+    expect(fib(3)).toBe(2);
+    expect(fib(6)).toBe(8);
+  });
 });
